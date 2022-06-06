a8bf2b2e311cfdbb2ccd729efee2fe9e39ad77ef
Will Kang <Williamjk5100@gmail.com>
2022-06-05 22:05:44 -0400

    Create AdminPage.js
    added admin page

---
+++ get-in-line/src/AdminPage.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import GetInLineTitle from './GetInLineTitle';
+
+
+const navigate = useNavigate();
+
+function goToTable() {
+    alert('taking you to queues');
+    navigate('/about')  
+}
+
+export default function AdminPage() {
+  return (
+    <div>
+      <GetInLineTitle/>
+              <button onClick={goToTable}>
+                  View Your Queues
+                  </button>
+              <button>
+                  Settings
+                  </button>
+              <button>
+                  Log Out
+                  </button>
+
+
+    </div>
+  )
+}
