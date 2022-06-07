19b544ac0f183645e8f9d05ce410526e0f0b42af
Lukman, Andrew Widjaja <alukman3@gatech.edu>
2022-06-07 01:08:41 -0400

    Implements navigation back and forth from Login<->UserPage using react-router-dom

--- get-in-line/src/components/LoginPage.js
+++ get-in-line/src/components/LoginPage.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import GetInLineTitle from './GetInLineTitle'
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
+  let navigate = useNavigate();
+
+
   return (
     <>
     <div>
       <GetInLineTitle/>
       Username <input type="text" /> <br/>
       Password <input type="text" /> <br/>
-      <button>User Login</button> 
-      <button>Admin Login</button>
+      <button 
+        onClick={() => {
+          navigate('/user');
+          }} >
+          User Login
+      </button>
+      <button>Admin Login</button> 
 
     </div>
     </>
