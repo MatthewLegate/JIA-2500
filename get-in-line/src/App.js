2d5e7bcb7619ebbce9f112c3110df11370663cce
Lukman, Andrew Widjaja <alukman3@gatech.edu>
2022-06-07 23:49:07 -0400

    Adds functionality to adminLogin and Logout buttons. Default home page is the login page

--- get-in-line/src/App.js
+++ get-in-line/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import AdminPage from './AdminPage';
 import LoginPage from './components/LoginPage';
 import User from './User';
 
@@ -8,8 +9,10 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path='' element={<LoginPage/>} />           /* Default localhost:3000 link goes to login */
         <Route exact path='/login' element={<LoginPage/>} />
         <Route exact path='/user' element={<User/>} />
+        <Route exact path='/admin' element={<AdminPage/>} />
       </Routes>
     </Router>
   )
--- get-in-line/src/components/LoginPage.js
+++ get-in-line/src/components/LoginPage.js
@@ -12,14 +12,20 @@ export default function LoginPage() {
       <GetInLineTitle/>
       Username <input type="text" /> <br/>
       Password <input type="text" /> <br/>
+      
       <button 
         onClick={() => {
           navigate('/user');
           }} >
           User Login
       </button>
-      <button>Admin Login</button> 
 
+      <button 
+        onClick={() => {
+          navigate('/admin');
+          }} >
+          Admin Login
+      </button> 
     </div>
     </>
   )
