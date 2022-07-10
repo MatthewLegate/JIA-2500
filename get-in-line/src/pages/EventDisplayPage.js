2046aaaca94c74947712b02139c4faf663fe6aae
Will Kang <Williamjk5100@gmail.com>
2022-07-10 16:46:17 -0400

    finished user-sided dequeuing

--- get-in-line/src/pages/EventDisplayPage.js
+++ get-in-line/src/pages/EventDisplayPage.js
@@ -57,50 +57,40 @@ export default function EventDisplayPage() {
             return;
         } else {
           console.log("hello");
-          removeUser(Event, Name, Email);
+          RemoveUser(Event, Name, Email);
         }
     }
 
-    async function removeUser(Event, UserName, UserEmail) {
-            //Initialize event document
-        const path = 'event/' + Event;
-        const event = doc(db, 'event', Event);
-        let Userqueue = [];
-        let Emailqueue = [];
-
-        //Query into firebase to read queue from document
-        const queuesQuery = query(
-          collection(db, 'event'),
-          limit(100) // Just to make sure we're not querying more than 100 events. Can be removed if database grows and is needed
-        );
-
-        const querySnapshot = await getDocs(queuesQuery);
-        const allDocs = querySnapshot.forEach((snap) => {
-          if (snap.data().name == Event) {
-            Userqueue = snap.data().userQueue;
-            Emailqueue = snap.data().emailQueue;
-          }
-        });
-
-        //remove user from queue
-        Userqueue = Userqueue.filter(function(UserName) {
-          alert("removing1");
-          return UserName !== UserName
-        })
-
-        Emailqueue = Emailqueue.filter(function(UserEmail) {
-          alert("removing2");
-          return UserEmail !== UserEmail
-        })
-
-        //update document with new queue and number of people
-        updateDoc(event, {
-          numOfPeople: Userqueue.length,
-          userQueue: Userqueue,
-          emailQueue: Emailqueue
-        });
-
-        alert("Removing " + UserName + " from " + Event);
+    async function RemoveUser(Event, UserName) {
+      //Initialize event document
+      const event = doc(db, 'event', Event);
+      let queue = [];
+  
+      //Query into firebase to read queue from document
+      const queuesQuery = query(
+        collection(db, 'event'),
+        limit(100) // Just to make sure we're not querying more than 100 events. Can be removed if database grows and is needed
+      );
+  
+      const querySnapshot = await getDocs(queuesQuery);
+      const allDocs = querySnapshot.forEach((snap) => {
+        if (snap.data().name == Event) {
+          queue = snap.data().queue;
+        }
+      });
+  
+      //remove user from queue
+      queue = queue.filter(function(name) {
+        return name !== UserName
+      })
+  
+      //update document with new queue and number of people
+      updateDoc(event, {
+        numOfPeople: queue.length,
+        queue: queue
+      });
+  
+      alert("Removing " + UserName + " from " + Event);
     }
 
     function verifyUserAdd() {
